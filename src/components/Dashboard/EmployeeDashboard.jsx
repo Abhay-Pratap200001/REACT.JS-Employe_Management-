bcc035e00d6d3933a799ec9e42d2724e6759b457
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-05-18 01:52:41 +0530

    Completed Admindasbord,Alltask, header TaskListnumber, TaskList, UI user interface is completed

---
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../other/Header'
+import TaskListNumbers from '../other/TaskListNumbers'
+import TaskList from '../TaskList/TaskList'
+
+const EmployeeDashboard = () => {
+  return (
+    <div className='p-20 bg-[#1C1C1C] h-screen'>
+      <h1></h1>
+      <Header/>
+      <TaskListNumbers/>
+      <TaskList/>
+    </div>
+  )
+}
+
+export default EmployeeDashboard
